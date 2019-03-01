c203987f8c29c0f0782ceb341babf1d4e50ff1f7
Lorris <lorris.canals@etu.univ-orleans.fr>
2019-03-01 14:55:07 -0500

    Affichage valeur écran Android
    Les valeurs récupérées via l'API s'affiche correctement sur l'écran de la machine virtuelle Android

--- Meteo/app/src/main/java/org/appducegep/macryptomonnaie/PageCryptomonnaie.java
+++ Meteo/app/src/main/java/org/appducegep/macryptomonnaie/PageCryptomonnaie.java
@@ -64,31 +64,6 @@ public class PageCryptomonnaie extends AppCompatActivity {
         System.out.println(jsonobj);
 
         try {
-            /*
-            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = null;
-            docBuilder = builderFactory.newDocumentBuilder();
-            Document doc = null;
-            doc = docBuilder.parse(new ByteArrayInputStream(xml.getBytes("UTF-8")));
-            Element elementHumidite = (Element)doc.getElementsByTagName("humidity").item(0);
-            String humidite = elementHumidite.getTextContent();
-            Element elementVentForce = (Element)doc.getElementsByTagName("wind_kph").item(0);
-            String ventForce = elementVentForce.getTextContent();
-            Element elementVentDirection = (Element)doc.getElementsByTagName("wind_dir").item(0);
-            String ventDirection = elementVentDirection.getTextContent();
-            Element elementCondition = (Element)doc.getElementsByTagName("condition").item(0);
-            Element elementSoleilOuNuage = (Element)elementCondition.getElementsByTagName("text").item(0);
-            String soleilOuNuage = elementSoleilOuNuage.getTextContent();
-            if(soleilOuNuage.compareTo("Sunny") == 0) soleilOuNuage = "Ensoleillé";
-            else soleilOuNuage = "Nuageux";
-            Element elementLieu = (Element)doc.getElementsByTagName("location").item(0);
-            Element elementVille = (Element)elementLieu.getElementsByTagName("name").item(0);
-            String ville = elementVille.getTextContent();
-            String vent = ventForce + ventDirection;
-            Element elementTemperature = (Element)doc.getElementsByTagName("temp_c").item(0);
-            float temperature = Float.parseFloat(elementTemperature.getTextContent());
-            */
-
             String symbol = jsonobj.getJSONObject("CAD").getString("symbol");
             String min15= jsonobj.getJSONObject("CAD").getString("15m");
             String last = jsonobj.getJSONObject("CAD").getString("last");
@@ -105,18 +80,21 @@ public class PageCryptomonnaie extends AppCompatActivity {
             System.out.println("/// Sell = " + sell);
             System.out.println("/////////////////////////////");
 
-            /*
+
             TextView affichageTitrePage = (TextView)this.findViewById(R.id.titre_page_meteo);
-            affichageTitrePage.setText("Météo de " + ville);
+            affichageTitrePage.setText("Cours du Bitcoin" );
+
 
             TextView affichageMeteo = (TextView)this.findViewById(R.id.meteo);
-            affichageMeteo.setText(soleilOuNuage + "\n");
+            affichageMeteo.setText("Symbol : " + symbol + "\n");
             affichageMeteo.append("\n\n");
-            affichageMeteo.append("Température : " + temperature + "\n");
-            affichageMeteo.append("Vent : " + vent + "\n");
-            affichageMeteo.append("Humidite : " + humidite + "\n");
+            affichageMeteo.append("15 min : " + min15 + "\n");
+            affichageMeteo.append("Last : " + last + "\n");
+            affichageMeteo.append("Buy : " + buy + "\n");
+            affichageMeteo.append("Sell : " + sell + "\n");
 
 
+            /*
             CryptomonnaieDAO meteoDAO = new CryptomonnaieDAO(getApplicationContext());
             meteoDAO.ajouterMeteo(soleilOuNuage, Integer.parseInt(humidite), vent, temperature);
             */
